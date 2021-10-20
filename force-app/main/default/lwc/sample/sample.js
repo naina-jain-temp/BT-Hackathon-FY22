0dd18a05ca7a805bce9712e95e37cd255b3f89c7
naina-jain-temp <vaishali23jain@gmail.com>
2021-10-20 09:44:04 +0530

    Update sample.js

--- force-app/main/default/lwc/sample/sample.js
+++ force-app/main/default/lwc/sample/sample.js
@@ -1,5 +1,5 @@
 import { LightningElement } from 'lwc';
-//hello comment check world data
+//hello comment check world data chnage
 export default class Sample extends LightningElement {
 
     data;
